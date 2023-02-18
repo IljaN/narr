e93cc534819e92d14475abe26d4c16679dd2ff41
Ilja Neumann <node512@gmail.com>
2023-02-18 11:36:05 +0100

    Reorder methods

--- main.go
+++ main.go
@@ -168,40 +168,6 @@ func connectToChromeDebugger(ctx context.Context, url string) (*cdp.Client, erro
 	return cdp.NewClient(conn), nil
 }
 
-// Media resources have the path format /range/0-nnnn...
-func isMediaURL(u string) bool {
-	return strings.Contains(u, "/range/0-")
-}
-
-// toDownloadableURL removes the path from the payload to make the resource downloadable. In our case the path
-// always contains a download-range in bytes which we can discard. See isMediaURL.
-func toDownloadableURL(audioURL string) string {
-	// We need to remove the path from the audio url to get a downloadable url
-	u, err := url.Parse(audioURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	u.Path = ""
-	return u.String()
-}
-
-// toDownloadPath returns the path where to download the audio file. The path is composed of the video id, the track
-// id and a random number.
-func toDownloadPath(videoURL string, downloadDir string) string {
-	u, err := url.Parse(videoURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if strings.HasPrefix(u.Path, "/watch") && u.Query().Has("trackId") {
-		videoId := strings.TrimLeft(u.Path, "/watch/")
-		trackId := u.Query().Get("trackId")
-		return downloadDir + "/" + videoId + "-" + trackId + "-" + strconv.Itoa(rand.Int())
-	}
-
-	return downloadDir + "/" + "DL-" + strconv.Itoa(rand.Int())
-}
-
 func enqueueDownload(q *queue.Queue, fromURL, toPath string) error {
 	go func(s, t string) {
 		err := q.QueueTask(func(ctx context.Context) error {
@@ -273,6 +239,40 @@ func probeFileFormat(body io.Reader, header []byte) (isAudio bool, err error) {
 	}
 
 	return rd.Ftyp.MajorBrand == "mp42", nil
+}
+
+// toDownloadableURL removes the path from the payload to make the resource downloadable. In our case the path
+// always contains a download-range in bytes which we can discard. See isMediaURL.
+func toDownloadableURL(audioURL string) string {
+	// We need to remove the path from the audio url to get a downloadable url
+	u, err := url.Parse(audioURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	u.Path = ""
+	return u.String()
+}
+
+// toDownloadPath returns the path where to download the audio file. The path is composed of the video id, the track
+// id and a random number.
+func toDownloadPath(videoURL string, downloadDir string) string {
+	u, err := url.Parse(videoURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if strings.HasPrefix(u.Path, "/watch") && u.Query().Has("trackId") {
+		videoId := strings.TrimLeft(u.Path, "/watch/")
+		trackId := u.Query().Get("trackId")
+		return downloadDir + "/" + videoId + "-" + trackId + "-" + strconv.Itoa(rand.Int())
+	}
+
+	return downloadDir + "/" + "DL-" + strconv.Itoa(rand.Int())
+}
+
+// Media resources have the path format /range/0-nnnn...
+func isMediaURL(u string) bool {
+	return strings.Contains(u, "/range/0-")
 }
 
 // runBatchFunc is the function signature for runBatch.
