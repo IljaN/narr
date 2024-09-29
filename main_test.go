254b81963ff5d0ed9177ac0455ce34a4b3d23984
Ilja Neumann <node512@gmail.com>
2024-09-29 04:22:24 +0200

    Test format probing

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
+	"os"
 	"strings"
 	"testing"
 )
@@ -16,4 +17,28 @@ func TestToDownloadPath(t *testing.T) {
 	videoUrl = "https://www.netflix.com"
 	downloadPath = toDownloadPath(videoUrl, downloadDir)
 	assert.True(t, strings.HasPrefix(downloadPath, "/tmp/foo/DL-"))
+}
+
+func TestProbeFileFormat(t *testing.T) {
+	testCases := []struct {
+		filePath string
+		isAudio  bool
+	}{
+		{"test/testdata/audio_iso6.mp4", true},
+		{"test/testdata/audio_mp42.mp4", true},
+		{"test/testdata/vid.mp4", false},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.filePath, func(t *testing.T) {
+			data, err := os.ReadFile(tc.filePath)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			isAudio, err := probeFileFormat(data)
+			assert.Nil(t, err)
+			assert.Equal(t, tc.isAudio, isAudio)
+		})
+	}
 }
